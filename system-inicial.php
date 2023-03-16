95d2cea9a58acff18047727f360f882165dd627c
Diego Lamana <126724083+diegoCodeJavascript@users.noreply.github.com>
2023-03-16 08:15:27 -0300

    Atualização importante
    Correção no link de navegação, e adicionado dois botões. 
    um botão de sair  e outro de acessar a configurações de conta. onde o funcionário pode, editar o cadastro, de acesso.

--- system-inicial.php
+++ system-inicial.php
@@ -20,7 +20,7 @@
 <body>
   <div id="cxcaixamenu">
     <h1 id='txtopcsystem'>Opções do Sistema</h1>
-    <a href=system-inicial><button id="btnpainel">Painel</button></a>
+    <a href=system-inicial.php><button id="btnpainel">Painel</button></a>
     <a href=cadastrar-pacientes.php><button id="btncadastrarpaciente">Cadastrar paciente</button></a>
     <a href=excluir-pacientes.php><button id="btnexcluirpaciente">Excluir paciente</button></a>
     <a href=lc-comparecimento.php><button id="btnlancarcomparecimento">Lançar comparecimento</button></a>
@@ -28,9 +28,10 @@
     <a href=consultar-cadastros.php><button id="consultarpacientes">Consultar paciente</button></a>
     
   </div>
-  <button id="btnsairsistema">Sair do sistema</button>
+  <a href=sair.php><button id="btnsairsistema">Sair do sistema</button></a>
   <!--Orientação ao dev/programador colocar um logo com/fundo transparente -->
   <img src="logo.png" id="logotelainicial" />
+  <a href=dconfigurar-conta.php><img src="system-imagens/55699f9gcofing.jpg" id="btncofigconta" /></a>
 <?php
   else:
   	  header("Location: log-erro.php");
